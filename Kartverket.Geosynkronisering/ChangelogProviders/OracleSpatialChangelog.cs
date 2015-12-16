913c18ea48d285e5eb2d2d6a20e61a1c049e15b1
larseggan_cp <bd8c31b7942bf043635817757d30dd2c83ffc3fbr4kMMRTU>
2015-12-16 10:08:10 +0000

    Refactoring: -Implemented abstract class SpatialDbChangelog. - PostGISChangelog now inherits SpatialDbChangelog. - Added wrapper classes SqlServerSpatialChangelog and OracleSpatialChangelog that inherits SpatialDbChangelog.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS12]$/geosyncronization/Geosynk/Development;C40622

---
+++ Kartverket.Geosynkronisering/ChangelogProviders/OracleSpatialChangelog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kartverket.Geosynkronisering.ChangelogProviders
+{
+    public class OracleSpatialChangelog : SpatialDbChangelog
+    {
+        //TODO: Implement Oracle Spatial provider
+        public override string GetLastIndex(int datasetId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void MakeChangeLog(int startChangeId, int count, string dbConnectInfo, string wfsUrl, string changeLogFileName, int datasetId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Kartverket.Geosynkronisering/ChangelogProviders/SqlServerSpatialChangelog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kartverket.Geosynkronisering.ChangelogProviders
+{
+    public class SqlServerSpatialChangelog : SpatialDbChangelog
+    {
+        //TODO: Implement SQL Server Spatial provider
+        public override string GetLastIndex(int datasetId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void MakeChangeLog(int startChangeId, int count, string dbConnectInfo, string wfsUrl, string changeLogFileName, int datasetId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
