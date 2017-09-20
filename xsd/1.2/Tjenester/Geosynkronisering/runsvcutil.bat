9ce3805bc5c1b65f86e38b32f9eb373d88f710c6
jarle pedersen <jarle.pedersen@norconsult.com>
2017-09-20 13:29:22 +0200

    added 1.2 folder for wsdl/xsd

---
+++ xsd/1.2/Tjenester/Geosynkronisering/runsvcutil.bat
@@ -0,0 +1,7 @@
+cd C:\Users\tor\Documents\Visual Studio 2010\Projects\Kartverket.Geosynkronisering\Kartverket.Geosynkronisering\Datacontract
+"C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\Bin\svcutil.exe" geosync.wsdl geosync2.xsd owsAll.xsd ows19115subset.xsd owsCommon.xsd owsContents.xsd owsDataIdentification.xsd owsDomainType.xsd owsExceptionReport.xsd owsGetCapabilities.xsd owsGetResourceByID.xsd owsInputOutputData.xsd owsManifest.xsd owsOperationsMetadata.xsd owsServiceIdentification.xsd owsServiceProvider.xsd http://www.w3.org/1999/xlink.xsd wfs.xsd sort.xsd query.xsd filterCapabilities.xsd filterAll.xsd filter.xsd expr.xsd /out:GeosyncWCFproxy.cs  /language:CS  /namespace:*,Kartverket.GeosyncWCF
+
+PAUSE
+cd C:\Users\tor\Documents\Visual Studio 2010\Projects\Kartverket.Geosynkronisering\Kartverket.Geosynkronisering\Datacontract
+"C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\Bin\svcutil.exe" /?
+PAUSE
