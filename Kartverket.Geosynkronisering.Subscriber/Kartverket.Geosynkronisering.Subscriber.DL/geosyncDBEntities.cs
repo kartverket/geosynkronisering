8e64db3916131ddde61869e8e4dc5adb550d5e0a
Lars Eggan <lars.eggan@norconsult.com>
2019-12-17 14:13:19 +0100

    BL & DL projects now .Net Standard 2.0. Subscriper .NET 4.7.2. Fixed nuget references.

--- Kartverket.Geosynkronisering.Subscriber/Kartverket.Geosynkronisering.Subscriber.DL/geosyncDBEntities.cs
+++ Kartverket.Geosynkronisering.Subscriber/Kartverket.Geosynkronisering.Subscriber.DL/geosyncDBEntities.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.SQLite;
 using System.IO;
 using System.Linq;
 using Dapper;
 using Dapper.Contrib.Extensions;
-using Microsoft.Data.Sqlite;
 
 namespace Kartverket.Geosynkronisering.Subscriber.DL
 {
@@ -17,10 +17,9 @@ namespace Kartverket.Geosynkronisering.Subscriber.DL
 
         public GeosyncDbEntities()
         {
-
             SqlMapperExtensions.TableNameMapper = type => type.Name;
 
-            ConnectionString = new SqliteConnectionStringBuilder(System.Configuration.ConfigurationManager.ConnectionStrings["geosyncDBEntities"].ConnectionString).ToString();
+            ConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["geosyncDBEntities"].ConnectionString;
 
             CreateDatabaseIfNotExists();
 
@@ -29,12 +28,14 @@ namespace Kartverket.Geosynkronisering.Subscriber.DL
 
         private static void CreateDatabaseIfNotExists()
         {
-            using (var Connection = new SqliteConnection(ConnectionString))
-                if (!File.Exists(Connection.DataSource))
+            var filePath = Directory.GetCurrentDirectory() + "\\" + ConnectionString.Split(';')[0].Split('=')[1];
+
+            using (var Connection = new SQLiteConnection(ConnectionString))
+                if (!File.Exists(filePath))
                 {
                     Connection.Open();
 
-                    using (var cmd = new SqliteCommand(File.ReadAllText("databaseSchema.sqlce"), Connection)) cmd.ExecuteNonQuery();
+                    using (var cmd = new SQLiteCommand(File.ReadAllText("databaseSchema.sqlce"), Connection)) cmd.ExecuteNonQuery();
                 }
         }
 
@@ -59,31 +60,29 @@ namespace Kartverket.Geosynkronisering.Subscriber.DL
 
         public static List<T> ReadAll<T>(string tableName)
         {
-
-            using (IDbConnection db = new SqliteConnection(ConnectionString))
+            using (IDbConnection db = new SQLiteConnection(ConnectionString))
                 return db.Query<T>("SELECT * FROM " + tableName).ToList();
-
         }
 
         public static bool DatasetExists(int datasetId)
         {
-            using (IDbConnection db = new SqliteConnection(ConnectionString))
+            using (IDbConnection db = new SQLiteConnection(ConnectionString))
                 return db.Query<int>($"SELECT 1 FROM Dataset WHERE DatasetId = {datasetId}").ToList().FirstOrDefault() == 1;
         }
 
         private static void InsertDataset(Dataset dataset)
         {
-            using (IDbConnection db = new SqliteConnection(ConnectionString)) db.Insert(dataset);
+            using (IDbConnection db = new SQLiteConnection(ConnectionString)) db.Insert(dataset);
         }
 
         private static void UpdateDataset(Dataset dataset)
         {
-            using (IDbConnection db = new SqliteConnection(ConnectionString)) db.Update(dataset);
+            using (IDbConnection db = new SQLiteConnection(ConnectionString)) db.Update(dataset);
         }
 
         public void DeleteObject(Dataset dataset)
         {
-            using (IDbConnection db = new SqliteConnection(ConnectionString)) db.Delete(dataset);
+            using (IDbConnection db = new SQLiteConnection(ConnectionString)) db.Delete(dataset);
 
             Dataset.Remove(dataset);
         }
