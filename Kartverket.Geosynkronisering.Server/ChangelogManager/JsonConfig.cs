16603d51ce4cbdc5c058cbfe1b88a6cc504aa47d
larseggan <lars.eggan@norconsult.com>
2022-10-20 16:44:04 +0200

    issue #158: Fixed: Etter publisering må det kunne fungre å kjøre Blazor Geosynk Push på IIS. Også teste publisering av Provider_Netcore.

--- Kartverket.Geosynkronisering.Server/ChangelogManager/JsonConfig.cs
+++ Kartverket.Geosynkronisering.Server/ChangelogManager/JsonConfig.cs
@@ -8,6 +8,7 @@ using System.Text;
 using Microsoft.Extensions.Configuration;
 //using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Configuration.Json;
+using Serilog;
 
 namespace ChangelogManager
 {
@@ -16,14 +17,31 @@ namespace ChangelogManager
 
         public static IConfiguration SetupJsonConfig(object bindTo = null)
         {
+            Log.Information("start SetupJsonConfig");
+
+            //var testGetCurrentDirectory = System.IO.Directory.GetCurrentDirectory();
+            //Log.Information("SetupJsonConfig testGetCurrentDirectory: {0}", testGetCurrentDirectory);
+
+
             // issue #157: fix for  self-contained executable, Assembly.GetEntryAssembly().Location failes
             var strPath = Process.GetCurrentProcess().MainModule.FileName;
+            Log.Information("SetupJsonConfig strPath: {0}", strPath);
             //var strPath = Assembly.GetEntryAssembly().Location; //AppDomain.CurrentDomain.BaseDirectory;
 
             var basePath = Path.GetDirectoryName(strPath);
-
+            Log.Information("SetupJsonConfig basePath: {0}", strPath);
             var strJsonFile = Path.Combine(basePath, "appsettings.json");
+            Log.Information("SetupJsonConfig strJsonFile: {0}", strPath);
 
+            if (!File.Exists(strJsonFile))
+            {
+                // When blazor-app is published on IIS, we must use this method to find the correct directory of where the project is stored
+                basePath = AppDomain.CurrentDomain.BaseDirectory;
+                Log.Information("SetupJsonConfig basePath: {0}", basePath);
+                strJsonFile = Path.Combine(basePath, "appsettings.json");
+                Log.Information("SetupJsonConfig strJsonFile: {0}", strJsonFile);
+
+            }
             var configuration = new ConfigurationBuilder()
                 .SetBasePath(basePath) // Directory where the json files are located
                 .AddJsonFile(strJsonFile, optional: false, reloadOnChange: true)
@@ -38,8 +56,8 @@ namespace ChangelogManager
 
             // test
             //var connectionstring = configuration.GetValue<string>("connectionStrings:geosyncEntities");
-            
 
+            Log.Information("SetupJsonConfig End ,configuration != null: {0}", configuration != null);
             return configuration;
         }
     }
