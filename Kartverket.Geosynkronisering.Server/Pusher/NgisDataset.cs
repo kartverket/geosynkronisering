8ffc6327be6b270d67670b435298ef7eefa4c9f0
Jarle Pedersen <Jarle.Pedersen@geodata.no>
2022-11-29 15:03:04 +0100

    use string for id

--- Kartverket.Geosynkronisering.Server/Pusher/NgisDataset.cs
+++ Kartverket.Geosynkronisering.Server/Pusher/NgisDataset.cs
@@ -4,7 +4,7 @@ namespace Provider_NetCore
 {
     public class NgisDataset
     {
-        public Guid id {get;set;}
+        public string id {get;set;}
         public string name {get;set;}
         public string access { get; set; }
     }
